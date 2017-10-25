5014fbfde3d225e64dad275cfe95a4f5ff3fc2a8
Kennan Zhong <kenzhong@gmail.com>
2017-10-24 23:04:19 -0400

    nav menu links turn to icons now on mobile

--- frontend/components/header/splash_header.jsx
+++ frontend/components/header/splash_header.jsx
@@ -7,8 +7,14 @@ const SplashHeader = () => (
       <div className='flex-center nav-component'>
         <Link to='/' className='nav-link-light nav-logo flex-center'>pxl-On</Link>
         <ul className='flex-center'>
-          <li><a className='nav-link-light' href='#'>Discover</a></li>
-          <li><a className='nav-link-light' href='#'>About</a></li>
+          <li><a className='nav-link-light' href='#'>
+            <i className='fa fa-picture-o' aria-hidden='true' />
+            <span className='nav-link-text'>Discover</span>
+          </a></li>
+          <li><a className='nav-link-light' href='#'>
+            <i className='fa fa-info-circle' aria-hidden='true' />
+            <span className='nav-link-text'>About</span>
+          </a></li>
         </ul>
       </div>
       <div className='flex-center nav-component'>
--- frontend/components/header/standard_header.jsx
+++ frontend/components/header/standard_header.jsx
@@ -7,8 +7,14 @@ const StandardHeader = (props) => (
       <div className='flex-center nav-component'>
         <Link to='/' className='nav-logo flex-center'>pxl-On</Link>
         <ul className='flex-center'>
-          <li><a className='nav-link' href='#'>Discover</a></li>
-          <li><a className='nav-link' href='#'>About</a></li>
+          <li><a className='nav-link' href='#'>
+            <i className='fa fa-picture-o' aria-hidden='true' />
+            <span className='nav-link-text'>Discover</span>
+          </a></li>
+          <li><a className='nav-link' href='#'>
+            <i className='fa fa-info-circle' aria-hidden='true' />
+            <span className='nav-link-text'>About</span>
+          </a></li>
         </ul>
       </div>
       <div className='flex-center nav-component'>
