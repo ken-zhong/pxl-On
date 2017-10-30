2795a450620e1352a860858cd9f03c31e2ec90e2
Kennan Zhong <kenzhong@gmail.com>
2017-10-30 16:10:44 -0400

    update s3 to https

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module PxlOn
     # -- all .rb files in that directory are automatically loaded.
     config.paperclip_defaults = {
       storage: :s3,
-      s3_protocol: :http,
+      s3_protocol: :https,
       s3_credentials: {
         bucket: ENV["s3_bucket"],
         access_key_id: ENV["s3_access_key_id"],
