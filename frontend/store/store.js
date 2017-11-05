cb1e701c4559bcfb00818f3341769d627ec70ae3
Kennan Zhong <kenzhong@gmail.com>
2017-11-05 16:04:38 -0500

    prepare for production deployment

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,10 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import RootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 
-const configureStore = (preloadedState = {}) => {
-  return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger));
-};
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
+const configureStore = (preloadedState = {}) => (
+  createStore(RootReducer, preloadedState, applyMiddleware(...middlewares))
+);
 
 export default configureStore;
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,33 @@
-var path = require("path");
+var path = require('path');
+var webpack = require('webpack');
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/pxl-On.jsx",
+  entry: './frontend/pxl-On.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: "bundle.js"
+    filename: 'bundle.js'
   },
+  plugins: plugins,
   module: {
     loaders: [
       {
@@ -21,6 +42,6 @@ module.exports = {
   },
   devtool: 'source-map',
   resolve: {
-    extensions: [".js", ".jsx", "*"]
+    extensions: ['.js', '.jsx', '*']
   }
 };
